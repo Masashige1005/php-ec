7ac3aa465de0520c260db5feb0878f54862b4c65
Masashige1005 <ineimasashige@gmail.com>
2020-02-18 22:15:14 +0900

    Add Noselect screen

--- htdocs/staff/staff_branch.php
+++ htdocs/staff/staff_branch.php
@@ -8,12 +8,21 @@
 	<?php
 	// 変数の中身を確認後それぞれのページへ移行
 	if(isset($_POST['edit']) == TRUE){
+		// 選択されない時の処理
+		if(isset($_POST['staffcode']) == FALSE){
+			header('Location:staff_ng.php');
+			exit();
+		}
 		$staff_code = $_POST['staffcode'];
 		// 移行する際にURLパラメータでスタッフコードを渡す
 		header('Location:staff_edit.php?staffcode='.$staff_code);
 		exit();
 	}
 	if(isset($_POST['delete']) == TRUE){
+		if(isset($_POST['staffcode']) == FALSE){
+			header('Location:staff_ng.php');
+			exit();
+		}
 		$staff_code = $_POST['staffcode'];
 		header('Location:staff_delete.php?staffcode='.$staff_code);
 		exit();
---
+++ htdocs/staff/staff_ng.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "UTF-8">
+<title></title>
+</head>
+<body>
+	スタッフが選ばれていません。<br />
+	<a href = "staff_list.php">戻る</a>
+</body>
