1664816a15c93b42136a17ba27765cf05941189d
Masashige1005 <ineimasashige@gmail.com>
2020-02-19 23:37:46 +0900

    Add Produtct add function

---
+++ htdocs/Product/pro_add_check.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "UTF-8">
+<title></title>
+</head>
+<body>
+	<?php
+	$pro_name = $_POST['name'];
+	$pro_price = $_POST['price'];
+
+	// 入力情報の安全対策
+	$pro_name = htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
+	$pro_price = htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
+
+	if($pro_name == ''){
+		print '商品名が入力されていません。<br />';
+	} else {
+		print '商品名:';
+		print $pro_name;
+		print '<br />';
+	}
+	// 半角かどうか確認
+	if($pro_price == ''){
+		print '価格を入力してください。<br />';
+	} else {
+		print '価格:';
+		print $pro_price;
+		print '円<br />';
+	}
+
+	if($pro_name == '' || $pro_price == ''){
+		print '<form>';
+		print '<input type = "button" onclick = "history.back()" value = "戻る">';
+		print '<form>';
+	} else {
+		// 暗号化
+		print '上記の商品を追加します。<br />';
+		print '<form method = "post" action = "pro_add_done.php">';
+		print '<input type = "hidden" name = "name" value = "'.$pro_name.'">';
+		print '<input type = "hidden" name = "price" value = "'.$pro_price.'">';
+		print '<br />';
+		print '<input type = "button" onclick = "history.back()" value = "戻る">';
+		print '<input type = "submit" value = "OK">';
+		print '</form>';
+	}
+	?>
+
+</body>
+</html>
---
+++ htdocs/Product/pro_add_done.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "UTF-8">
+<title></title>
+</head>
+<body>
+	<?php
+	try{
+		$pro_name = $_POST['name'];
+		$pro_price = $_POST['price'];
+
+		// 入力情報の安全対策
+		$pro_name = htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
+		$pro_price = htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
+
+		// データベースへの接続
+		$dsn = 'mysql:dbname=app-db;host=localhost;charset=utf8';
+		$user = 'root';
+		$password = '';
+		$dbh = new PDO($dsn,$user,$password);
+		$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		// SQLを使いレコードを追加
+		$sql = 'INSERT INTO mst_product(name,price)VALUES(?,?)';
+		$stmt = $dbh->prepare($sql);
+		$data[] = $pro_name;
+		$data[] = $pro_price;
+		$stmt->execute($data);
+
+		// データベースから切断（ここ重要）
+		$dbh = null;
+
+		print $pro_name;
+		print 'を追加しました。<br />';
+	}
+
+	// データベースの障害が発生した時の処理
+	catch (Exception $e){
+		print 'ただいま障害により大変ご迷惑をお掛けしております。';
+		exit();
+	}
+	?>
+	<a href = "pro_list.php">戻る</a>
+
+</body>
+</html>
