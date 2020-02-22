58837aa94a3c824a7501b289ded54e5d7f250eaa
Masashige1005 <ineimasashige@gmail.com>
2020-02-22 20:07:34 +0900

    Add product dispimage

--- htdocs/Product/pro_disp.php
+++ htdocs/Product/pro_disp.php
@@ -15,7 +15,7 @@
 	$dbh = new PDO($dsn,$user,$password);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	// データベースから選択された名前を取得
-	$sql = 'SELECT name,price FROM mst_product WHERE code=?';
+	$sql = 'SELECT name,price,image FROM mst_product WHERE code=?';
 	$stmt = $dbh->prepare($sql);
 	$data[] = $pro_code;
 	$stmt->execute($data);
@@ -23,8 +23,14 @@
 	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
 	$pro_name = $rec['name'];
 	$pro_price = $rec['price'];
+	$pro_image_name = $rec['image'];
 
 	$dbh = null;
+	if($pro_image_name == ''){
+		$disp_image = '';
+	} else {
+		$disp_image = '<img src = "./image/'.$pro_image_name.'">';
+	}
 	}
 
 	// データベースの障害が発生した時の処理
@@ -44,6 +50,8 @@
 	<br />
 	価格<br />
 	<?php print $pro_price; ?> 円
+	<br />
+	<?php print $disp_image ?>
 	<br />
 	<br />
 	<form>
