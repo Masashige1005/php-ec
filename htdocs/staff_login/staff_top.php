1a3df2713eeba1c278825d2c81719fd774ea8cd6
Masashige1005 <ineimasashige@gmail.com>
2020-02-23 23:54:12 +0900

    Add staff login

---
+++ htdocs/staff_login/staff_top.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "UTF-8">
+<title></title>
+<body>
+	ショップ管理トップ<br />
+	<br />
+	<a href = "../staff/staff_list.php">スタッフ管理</a><br />
+	<br />
+	<a href = "../product/pro_list.php">商品管理</a>
+</body>
+</head>
+</html>
