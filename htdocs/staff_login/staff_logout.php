d6eae155c10a93b35ab4077753a3e80d7ccfed37
Masashige1005 <ineimasashige@gmail.com>
2020-02-24 23:01:16 +0900

    Add logout comment

--- htdocs/staff_login/staff_logout.php
+++ htdocs/staff_login/staff_logout.php
@@ -1,9 +1,13 @@
+
 <?php
 session_start();
+// セッション変数を空にする
 $_SESSION = array();
 if(isset($_COOKIE[session_name()]) == TRUE){
+	// セッションIDをクッキーから削除、setcookie前に画面表示があるといけない。
 	setcookie(session_name(),'',time()-42000,'/');
 }
+// セッションを破棄
 session_destroy();
 ?>
 <!DOCTYPE html>
