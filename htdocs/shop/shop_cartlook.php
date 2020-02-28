7f3363dbf61ac0ee3ca1fd587a9fbaa820f38519
Masashige1005 <ineimasashige@gmail.com>
2020-02-28 15:22:41 +0900

    Add Purchase check

--- htdocs/shop/shop_cartlook.php
+++ htdocs/shop/shop_cartlook.php
@@ -71,18 +71,32 @@ if(isset($_SESSION['member_login']) == FALSE){
 	カートの中身<br />
 	<br />
 	<form method = "post" action = "quan_change.php">
-	<?php for($i=0;$i<$max;$i++){ ?>
-		<?php print $pro_name[$i]; ?>
-		<?php print $pro_image[$i]; ?>
-		<?php print $pro_price[$i].'円'; ?>
-		<input type = "text" name = "quan<?php print $i;?>" value = "<?php print $quan[$i];?>">
-		<?php print $pro_price[$i]*$quan[$i];?>円
-		<input type = "checkbox" name = "delete<?php print $i;?>">
-		<?php print '<br />'; ?>
-	<?php } ?>
-		<input type = "hidden" name = "max" value = "<?php print $max; ?>">
-		<input type = "submit" value = "数値変更">
-		<input type = "button" onclick = "history.back()" value = "戻る">
+		<table border = "1">
+			<tr>
+				<td>商品</td>
+				<td>商品画像</td>
+				<td>価格</td>
+				<td>数量</td>
+				<td>小計</td>
+				<td>削除</td>
+			</tr>
+			<?php for($i=0;$i<$max;$i++){ ?>
+			<tr>
+				<td><?php print $pro_name[$i]; ?></td>
+				<td><?php print $pro_image[$i]; ?></td>
+				<td><?php print $pro_price[$i].'円'; ?></td>
+				<td><input type = "text" name = "quan<?php print $i;?>" value = "<?php print $quan[$i];?>"></td>
+				<td><?php print $pro_price[$i]*$quan[$i];?>円</td>
+				<td><input type = "checkbox" name = "delete<?php print $i;?>"></td>
+				<?php print '<br />'; ?>
+			</tr>
+			<?php } ?>
+		</table>
+			<input type = "hidden" name = "max" value = "<?php print $max; ?>">
+			<input type = "submit" value = "数値変更">
+			<input type = "button" onclick = "history.back()" value = "戻る">
 	</form>
+	<br />
+	<a href = "shop_form.html">購入手続きへ進む</a><br />
 </body>
 </html>
